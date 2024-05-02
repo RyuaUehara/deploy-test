b1ec3f2a0cdb3ffe4260f793af32e08e818ba58a
TakaraHideto <t-hideto-2411@occ.co.jp>
2024-05-02 12:27:17 +0900

    a

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
 
-    <main>bokuha takara hideto desu</main>
+    <main>bokuha takara hideto desu unnti</main>
   
   );
 }
